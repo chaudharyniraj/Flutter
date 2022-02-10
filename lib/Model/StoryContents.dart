b43f03f5060a2251f525bd2d2bee866f23737100
chaudharyniraj <nirajchaudhary106@gmail.com>
2022-02-10 18:31:41 +0600

    working on list view

---
+++ lib/Model/StoryContents.dart
@@ -0,0 +1,25 @@
+class StoryContents {
+  String image;
+  String name;
+
+  StoryContents({required this.name, required this.image});
+}
+
+List<StoryContents> contents = [
+  StoryContents(
+    name: 'Ram Chapagain',
+    image: 'image1.jpg',
+  ),
+  StoryContents(
+    name: 'Sagar Karki',
+    image: 'image2.jpg',
+  ),
+  StoryContents(
+    name: 'Rupesh Shrestha',
+    image: 'image1.jpg',
+  ),
+  StoryContents(
+    name: 'Kushal Rajbhandari',
+    image: 'image2.jpg',
+  ),
+];
--- lib/UI/TabbarUI/NewsfeedUI.dart
+++ lib/UI/TabbarUI/NewsfeedUI.dart
@@ -1,3 +1,4 @@
+import 'package:fb_design/Model/StoryContents.dart';
 import 'package:flutter/material.dart';
 
 class NewsfeedUI extends StatefulWidget {
@@ -8,11 +9,68 @@ class NewsfeedUI extends StatefulWidget {
 }
 
 class _NewsfeedUIState extends State<NewsfeedUI> {
+  ScrollController _scrollController = ScrollController();
+
+  Widget ListofStories(BuildContext context, int index) {
+    return Stack(
+      children: [
+        Image.asset('${contents[index].image}'),
+        Text("${contents[index].name}")
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return SingleChildScrollView(
       child: Column(
-        children: [Row()],
+        children: [
+          SizedBox(height: height * 0.01),
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Image.asset(
+                  'profile.png',
+                  width: width * 0.1,
+                  height: width * 0.1,
+                ),
+              ),
+              Expanded(
+                flex: 4,
+                child: TextFormField(
+                    readOnly: true,
+                    onTap: () {},
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      fillColor: Colors.black,
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 05.0, horizontal: 5.0),
+                      hintStyle: TextStyle(color: Colors.grey[800]),
+                      hintText: "Search",
+                    )),
+              )
+            ],
+          ),
+          Container(
+            child: ListView.builder(
+              controller: _scrollController,
+              // scrollDirection: Axis.horizontal,
+              itemCount: contents.length,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return Container(
+                    height: height * 0.2,
+                    width: width * 0.3,
+                    child: ListofStories(context, index));
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
