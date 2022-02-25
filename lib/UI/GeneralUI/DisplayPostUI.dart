e0320d34a994ef37bc299a06501b2185fc17c667
chaudharyniraj <nirajchaudhary106@gmail.com>
2022-02-25 14:02:31 +0600

    rearranging the coading structure

--- lib/UI/GeneralUI/DisplayPostUI.dart
+++ lib/UI/GeneralUI/DisplayPostUI.dart
@@ -21,39 +21,51 @@ class _DisplayPostUIState extends State<DisplayPostUI> {
       child: Scaffold(
         body: Stack(
           children: [
-            Container(
-              height: height * 0.98,
-              width: width,
-              child: Image.asset(
-                arguments.image,
-                fit: BoxFit.fitWidth,
-              ),
-            ),
-            IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: const Icon(Icons.arrow_back)),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(arguments.name),
-                    Text(arguments.caption),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(arguments.likes),
-                        Text(arguments.comments),
-                      ],
-                    )
-                  ]),
-            )
+            imageView(width, height, arguments),
+            backButton(),
+            records(arguments)
           ],
         ),
       ),
+    );
+  }
+
+  Widget imageView(width, height, arguments) {
+    return Container(
+      height: height * 0.98,
+      width: width,
+      child: Image.asset(
+        arguments.image,
+        fit: BoxFit.fitWidth,
+      ),
+    );
+  }
+
+  Widget backButton() {
+    return IconButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: const Icon(Icons.arrow_back));
+  }
+
+  Widget records(arguments) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(arguments.name),
+            Text(arguments.caption),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(arguments.likes),
+                Text(arguments.comments),
+              ],
+            )
+          ]),
     );
   }
 }
